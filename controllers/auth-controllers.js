0ab8d23efdd2a3044d49e20dbfb5d8152204ec0b
Tanju Özer <tanjuozer64@gmail.com>
2024-02-23 23:34:41 +0100

    handle auth controllers and models

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,9 +1,42 @@
+const UserModel = require("../models/UserModel");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequest, UnauthenticatedError } = require("../errors");
+
 const register = async (req, res) => {
-  res.send("register");
+  const { name, email, password } = req.body;
+
+  const user = await UserModel.create({ name, email, password });
+  res.status(StatusCodes.CREATED).json({ msg: "user created" });
 };
 
 const login = async (req, res) => {
-  res.send("login");
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequest("Please provide email and password");
+  }
+
+  const user = await UserModel.findOne({ email });
+
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  const token = user.createJWT();
+  console.log(token);
+
+  res.status(StatusCodes.OK).json({
+    user: {
+      email: user.email,
+      name: user.name,
+      token,
+    },
+  });
 };
 
 module.exports = { register, login };
