32359aa06cc796320f787f566f891a045328ad9a
Tanju Özer <tanjuozer64@gmail.com>
2024-02-24 01:04:58 +0100

    handle review model

---
+++ models/ReviewModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const ReviewSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  review: {
+    type: String,
+    required: true,
+  },
+  imdbID: {
+    type: String,
+    required: true,
+  },
+  poster: {
+    type: String,
+    required: true,
+  },
+  userRating: {
+    type: String,
+    required: true,
+  },
+  createdBy: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+});
+
+module.exports = mongoose.model("Review", ReviewSchema);
